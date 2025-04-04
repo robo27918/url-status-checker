2f8fe0adc3eb31f37569aac2c410576fb5f8f3c3
Roberto Toribio <roberttoribio04@gmail.com>
2025-04-04 15:51:19 -0700

    add input validation

--- public/main.js
+++ public/main.js
@@ -10,15 +10,25 @@ addInputBtn === null || addInputBtn === void 0 ? void 0 : addInputBtn.addEventLi
     let inputBoxStyle = "urlInput border text-xl border-gray-700 rounded-full bg-gray-100 w-90 h-12 block";
     let inputBox = document.createElement("input");
     inputBox.setAttribute("class", inputBoxStyle);
-    inputBox.setAttribute("type", "text");
+    inputBox.setAttribute("type", "url");
+    inputBox.required = true;
+    inputBox.setAttribute("pattern", "https?:\/\/[^\s/$.?#].[^\s]*");
     inputContainer === null || inputContainer === void 0 ? void 0 : inputContainer.appendChild(inputBox);
 });
-getStatusBtn === null || getStatusBtn === void 0 ? void 0 : getStatusBtn.addEventListener('click', () => {
+getStatusBtn === null || getStatusBtn === void 0 ? void 0 : getStatusBtn.addEventListener("click", () => {
     console.log("getStatus btn clicked");
-    //TODO: find a way to validate user Input 
+    //TODO: find a way to validate user Input
     let inputElements = document.getElementsByClassName("urlInput");
     console.log("all inputElements", inputElements);
-    //map elements to extract textContent
+    //Implement Client-side validation
+    for (let i = 0; i < inputElements.length; i++) {
+        //@ts-ignore
+        if (!inputElements[i].checkValidity()) {
+            //@ts-ignore
+            alert(`URL: ${inputElements[i].value} has invalid format`);
+            return;
+        }
+    }
     //@ts-ignore: input element does indeed have a value attribute :)
     let inputUrls = Array.from(inputElements).map((element) => element.value);
     console.log("values:", inputUrls);
--- public/main.ts
+++ public/main.ts
@@ -1,29 +1,39 @@
-console.log("hello from main.ts")
+console.log("hello from main.ts");
 //Getting necessary DOM elements
 let addInputBtn = document.getElementById("addInputBtn");
-let  getStatusBtn = document.getElementById("getStatusBtn");
+let getStatusBtn = document.getElementById("getStatusBtn");
 
-let inputContainer = document.getElementById("inputContainer")
+let inputContainer = document.getElementById("inputContainer");
 
 //Button event listeners
-addInputBtn?.addEventListener("click",()=>{
-    console.log("addInputButton clicked")
-    let inputBoxStyle = "urlInput border text-xl border-gray-700 rounded-full bg-gray-100 w-90 h-12 block";
-    let inputBox = document.createElement("input")
-    inputBox.setAttribute("class",inputBoxStyle);
-    inputBox.setAttribute("type","text") 
-    inputContainer?.appendChild(inputBox);
-})
+addInputBtn?.addEventListener("click", () => {
+  console.log("addInputButton clicked");
+  let inputBoxStyle =
+    "urlInput border text-xl border-gray-700 rounded-full bg-gray-100 w-90 h-12 block";
+  let inputBox = document.createElement("input");
+  inputBox.setAttribute("class", inputBoxStyle);
+  inputBox.setAttribute("type", "url");
+  inputBox.required = true;
+  inputBox.setAttribute("pattern", "https?:\/\/[^\s/$.?#].[^\s]*");
+  inputContainer?.appendChild(inputBox);
+});
 
-getStatusBtn?.addEventListener('click',()=>{
-    console.log("getStatus btn clicked")
-    //TODO: find a way to validate user Input 
-    let inputElements = document.getElementsByClassName("urlInput");
-    console.log("all inputElements", inputElements)
-    //map elements to extract textContent
+getStatusBtn?.addEventListener("click", () => {
+  console.log("getStatus btn clicked");
+  //TODO: find a way to validate user Input
+  let inputElements = document.getElementsByClassName("urlInput");
+  console.log("all inputElements", inputElements);
 
-    //@ts-ignore: input element does indeed have a value attribute :)
-    let inputUrls = Array.from(inputElements).map((element)=>element.value)
-    console.log("values:",inputUrls)
-    
-})
+  //Implement Client-side validation
+  for (let i = 0; i < inputElements.length; i++) {
+    //@ts-ignore
+    if (!inputElements[i].checkValidity()) {
+      //@ts-ignore
+      alert(`URL: ${inputElements[i].value} has invalid format`);
+      return;
+    }
+  }
+  //@ts-ignore: input element does indeed have a value attribute :)
+  let inputUrls = Array.from(inputElements).map((element) => element.value);
+  console.log("values:", inputUrls);
+});
